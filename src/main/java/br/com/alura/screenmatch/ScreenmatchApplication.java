e5e34ae0a2fde093161ab16628a1df1af740eae2
yurimaretti <yurimaretti@hotmail.com>
2024-06-13 15:20:59 -0300

    Primeiro commit

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,32 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ChamadaAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var chamada = new ChamadaAPI();
+		var json = chamada.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=c33b4e73");
+		System.out.println(json);
+
+//		json = chamada.obterDados("https://coffee.alexflipnote.dev/random.json");
+//		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+
+		System.out.println(dados);
+	}
+}
