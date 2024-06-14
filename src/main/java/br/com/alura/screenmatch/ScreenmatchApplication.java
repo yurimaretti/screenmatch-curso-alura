2c5087f72c59fd6875f0180b4ecedff3433078ef
yurimaretti <yurimaretti@hotmail.com>
2024-06-14 17:59:41 -0300

    Classe DadosEpisodio implementada

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ChamadaAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
@@ -24,9 +25,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 //		System.out.println(json);
 
 		ConverteDados conversor = new ConverteDados();
+		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
 
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-
-		System.out.println(dados);
+		json = chamada.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=c33b4e73");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
 	}
 }
