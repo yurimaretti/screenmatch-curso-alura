716a4c264995d96f86d391b80edd51089b1d23e1
yurimaretti <yurimaretti@hotmail.com>
2024-06-29 10:18:57 -0300

    Criado endpoint para listar series, com a classe SerieDTO, configuração do CORS e Live Reload

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,13 +1,34 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repository;
+
     @GetMapping("/series")
-    public String obterSeries() {
-        return "Aqui serão listadas as séries";
+    public List<SerieDTO> obterSeries() {
+        return repository.findAll().stream()
+                .map(s -> new SerieDTO(
+                        s.getId(),
+                        s.getTitulo(),
+                        s.getTotalTemporadas(),
+                        s.getAvaliacao(),
+                        s.getAtores(),
+                        s.getGenero(),
+                        s.getSinopse(),
+                        s.getPoster()
+                ))
+                .collect(Collectors.toList());
     }
 }
